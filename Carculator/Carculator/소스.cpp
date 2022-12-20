21155109dd1f359fedd49af7639cb3dac5f32180
BeBunny12 <kmr04121002@gmail.com>
2022-12-20 11:46:44 +0900

    asdf

--- Carculator/Carculator/소스.cpp
+++ Carculator/Carculator/소스.cpp
@@ -29,7 +29,8 @@ int main()
 			ch = '/';
 			ans = (first + 1) / (second + 1);
 			break;
-		
+		default:
+			break;
 		}
 		printf("%d %c %d = ", (first + 1), ch, (second + 1));
 		scanf("%d", &input);
